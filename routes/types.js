5a074f978b2d528cc2673fe6a9d894b4236d49f2
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2021-01-17 00:59:29 +0700

    add properties

--- routes/types.js
+++ routes/types.js
@@ -3,11 +3,6 @@ const router = express.Router();
 const db = require('../db/db');
 const { validate } = require('jsonschema');
 
-// const types = [
-//   { id: 1, title: 'Расходы', combined: true },
-//   { id: 2, title: 'Доходы', combined: true },
-// ];
-
 router.get('/', (req, res, next) => {
   const profit = db.get('profit');
   const expense = db.get('expense');
@@ -21,15 +16,18 @@ router.get('/:id', (req, res, next) => {
 
   res.json({ status: 'ok', data });
 });
-
 router.post('/', (req, res, next) => {
   const { body } = req;
+  console.log('body', body);
   const typeSchema = {
     type: 'object',
     properties: {
-      title: { type: 'string' },
+      categoryExpense: { type: 'string' },
+      commentExpense: { type: 'string' },
+      categoryProfit: { type: 'string' },
+      commentProfit: { type: 'string' },
     },
-    required: ['title'],
+    required: ['categoryProfit'],
     additionalProperties: false,
   };
 
@@ -38,15 +36,30 @@ router.post('/', (req, res, next) => {
     return next(new Error('INVALID_JSON_OR_API_FORMAT'));
   }
 
-  const newType = { id: 3, title: body.title, combined: false };
+  // const newType = { id: 3, title: body.title, combined: false };
+  const newExpense = {
+    id: 2,
+    totalExpense: 5000,
+    categoryExpense: body.categoryExpense,
+    commentExpense: 'аванс',
+    dateExpense: new Date().toLocaleDateString(),
+    combined: false,
+  };
+  const newProfit = {
+    id: 3,
+    totalProfit: 100,
+    categoryProfit: body.categoryProfit,
+    commentProfit: 'купил билеты',
+    dateProfit: new Date().toLocaleDateString(),
+    combined: false,
+  };
   try {
-    db.get('expense').push(newType).write();
-    db.get('profit').push(newType).write();
+    db.get('expense').push(newExpense).write();
+    db.get('profit').push(newProfit).write();
   } catch (error) {
     throw new Error(error);
   }
-
-  res.json({ status: 'ok', data: newType });
+  res.json({ status: 'ok', data: newProfit });
 });
 
 module.exports = router;
