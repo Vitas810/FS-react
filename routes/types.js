c0b5cf6e8d39713353a3108a61248ed336a348e3
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2021-01-17 17:59:07 +0700

    изменение путей, добавление удаления по id

--- routes/types.js
+++ routes/types.js
@@ -12,26 +12,25 @@ router.get('/', (req, res, next) => {
 //types/:id
 router.get('/:type/:id', (req, res, next) => {
   const { type, id } = req.params;
+  const typeElem = db.get(type);
+  const data = typeElem.find((type) => String(type.id) === id);
+  res.json({ status: 'ok', data });
+});
 
-  switch (type) {
-    case 'profit':
-      const profit = db.get('profit');
-      const dataProfit = profit.find((type) => String(type.id) === id);
-      res.json({ status: 'ok', dataProfit });
-      break;
-    case 'expense':
-      const expense = db.get('expense');
-      const dataExpense = expense.find((type) => String(type.id) === id);
-      res.json({ status: 'ok', dataExpense });
-      break;
-    default:
-      res.json({ status: 'Error, undefined type' });
-  }
+//delete
+router.delete('/:type/:id', (req, res, next) => {
+  const { type, id } = req.params;
+
+  db.get(type)
+    .remove({ id: Number(id) })
+    .write();
+
+  res.json({ status: 'ok' });
 });
 
 router.post('/', (req, res, next) => {
   const { body } = req;
-  console.log('body', body);
+
   const typeSchema = {
     type: 'object',
     properties: {
@@ -52,18 +51,15 @@ router.post('/', (req, res, next) => {
   const newElement = {
     id: 2,
     type: body.type,
-    total: 5000,
+    total: body.total,
     category: body.category,
-    comment: 'аванс',
+    comment: body.comment,
     date: new Date().toLocaleDateString(),
     combined: false,
   };
 
   try {
-    console.log('i am here');
-    body.type === 'Доход'
-      ? db.get('profit').push(newElement).write()
-      : db.get('expense').push(newElement).write();
+    db.get(body.type).push(newElement).write();
   } catch (error) {
     throw new Error(error);
   }
