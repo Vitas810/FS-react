95d536da4907e4b65d8799a4158ce2df70b1704f
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2021-01-31 23:18:55 +0700

    исправление кода

--- controllers/controlls.js
+++ controllers/controlls.js
@@ -6,28 +6,20 @@ const getProfit = (req, res, next) => {
   const data = db.get('profit');
   res.json({ status: 'ok', data });
 };
+const getExpenses = (req, res, next) => {
+  const data = db.get('expense');
+  res.json({ status: 'ok', data });
+};
+
 const getCategory = (req, res, next) => {
   const data = db.get('category');
   res.json({ status: 'ok', data });
 };
 
-const getType = (req, res, next) => {
-  const { type, id } = req.params;
-  const typeElem = db.get(type);
-  const data = typeElem.find((type) => String(type.id) === id);
-  res.json({ status: 'ok', data });
-};
-/* const getCategory = (req, res, next) => {
-  const { type, id } = req.params;
-  const typeElem = db.get(type);
-  const data = typeElem.find((type) => String(type.id) === id);
-  res.json({ status: 'ok', data });
-};
- */
 const createType = (req, res, next) => {
   const { body } = req;
   const types = body.inputHeaderValue;
-  console.log('typebody', body);
+
   const typeSchema = {
     type: 'object',
     properties: {
@@ -38,7 +30,7 @@ const createType = (req, res, next) => {
       srcImgCat: { type: 'string' },
       colorCategor: { type: 'string' },
     },
-    required: ['total'],
+    required: ['total', 'categoryValue'],
     additionalProperties: false,
   };
 
@@ -61,65 +53,22 @@ const createType = (req, res, next) => {
 
   try {
     db.get(types).push(newElement).write();
-    console.log('bodyTry', body);
   } catch (error) {
     throw new Error(error);
   }
   res.json({ status: 'ok', data: newElement });
 };
 
-const createCat = (req, res, next) => {
-  const { body } = req;
-
-  const typeSchema = {
-    type: 'object',
-    properties: {
-      type: { type: 'string' },
-      category: { type: 'string' },
-    },
-    required: ['type', 'category'],
-    additionalProperties: false,
-  };
-  const validationResult = validate(body, typeSchema);
-
-  if (body.type != 'category') return next(new Error('INVALID_TYPE'));
-  if (!validationResult.valid)
-    return next(new Error('INVALID_JSON_OR_API_FORMAT_CATEGORY'));
-
-  const newCat = {
-    id: shortid.generate(),
-    type: body.type,
-    category: body.category,
-    combined: false,
-  };
-
-  try {
-    db.get('profit').push(newCat).write();
-  } catch (error) {
-    throw new Error(error);
-  }
-  res.json({ status: 'ok', data: newCat });
-};
-
-const editTask = (req, res, next) => {
-  const { id, type } = req.params;
-  const editedTask = db.get(type).find({ id }).assign(req.body).value();
-  db.write();
-
-  res.json({ status: 'OK', data: editedTask });
-};
-
 const deleteType = (req, res, next) => {
-  db.get('profit').remove({ id: req.params.id }).write();
-
+  const { profit } = req.params;
+  db.get(profit).remove({ id: req.params.id }).write();
   res.json({ status: 'OK' });
 };
+
 module.exports = {
   getProfit,
-  getType,
   getCategory,
   deleteType,
   createType,
-  editTask,
-  createCat,
+  getExpenses,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const typeControlls = require('../controllers/controlls');
 
 router.post('/profit', typeControlls.createType);
-router.post('/category', typeControlls.createCat);
+router.post('/expense', typeControlls.createType);
 router.get('/profit', typeControlls.getProfit);
+router.get('/expense', typeControlls.getExpenses);
 router.get('/category', typeControlls.getCategory);
-router.get('/:type/:id', typeControlls.getType);
-router.get('/:type/:id', typeControlls.editTask);
-router.get('/:type/:id', typeControlls.getCategory);
 router.delete('/:profit/:id', typeControlls.deleteType);
 
 module.exports = router;
